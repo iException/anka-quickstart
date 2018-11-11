aac4cc35569b709d4365c6f838d3f474fe714af2
HeZaichang.WeRDyin <heyin223@163.com>
2018-11-11 21:43:18 +0800

    Update usage

--- anka.config.js
+++ anka.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   pages: 'pages',
-  components: 'components'
+  components: 'components',
+  template: {
+    page: './anka_template/pages',
+    component: './anka_template/components'
+  }
 };
